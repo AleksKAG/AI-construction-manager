8c1898bb15ad7da5856259273085e917154d2b53
AleksKAG <WyrkaloW@yandex.ru>
2026-02-06 12:28:26 +0300

    Create models.go

---
+++ internal/models/models.go
@@ -0,0 +1,22 @@
+package models
+
+type ProjectObject struct {
+	ID            string            `gorm:"primaryKey" json:"id"`
+	Name          string            `json:"name"`
+	Characteristics map[string]string `gorm:"type:jsonb" json:"characteristics"`
+	CostEstimates map[string]float64 `gorm:"type:jsonb" json:"cost_estimates"`
+}
+
+type GanttTask struct {
+	ID        string `gorm:"primaryKey" json:"id"`
+	ObjectID  string `json:"object_id"`
+	Name      string `json:"name"`
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
+	Duration  int    `json:"duration"`
+}
+
+type ProjectGraph struct {
+	ObjectID string      `json:"object_id"`
+	Tasks    []GanttTask `json:"tasks"`
+}
